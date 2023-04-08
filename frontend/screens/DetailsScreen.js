4f428e272225d949ce129c8d1979bd2fc44ade49
Lindsay <lindsay.chen@yale.edu>
2023-04-08 16:54:59 -0400

    updated

--- frontend/screens/DetailsScreen.js
+++ frontend/screens/DetailsScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 const DetailsScreen = ({ route }) => {
@@ -7,17 +7,67 @@ const DetailsScreen = ({ route }) => {
   const navigation = useNavigation();
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <View style={{ position: 'absolute', top: 20, left: 20 }}>
-        <TouchableOpacity onPress={() => navigation.goBack()}>
-          <Text style={{ fontSize: 16, fontWeight: 'bold', color: '#0c2340' }}>Back to Home</Text>
-        </TouchableOpacity>
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backButton}>
+        <Text style={styles.backButtonText}>Back to Home</Text>
+      </TouchableOpacity>
+      <View style={styles.contentContainer}>
+        <Text style={styles.title}>{post.title}</Text>
+        <Text style={styles.author}>By {post.author}</Text>
+        <Text style={styles.content}>{post.content}</Text>
+        <Text style={styles.rent}>Rent: {post.rent}</Text>
       </View>
-      <Text style={{ fontWeight: 'bold', fontSize: 20, marginTop: 40 }}>{post.title}</Text>
-      <Text style={{ marginTop: 10, color: '#0c2340' }}>{post.description}</Text>
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f2f2f2',
+    paddingHorizontal: 20,
+    paddingTop: 40,
+  },
+  backButton: {
+    position: 'absolute',
+    top: 20,
+    left: 20,
+    zIndex: 1,
+  },
+  backButtonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#0c2340',
+  },
+  contentContainer: {
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 20,
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#0c2340',
+    marginBottom: 10,
+  },
+  author: {
+    fontSize: 16,
+    color: '#0c2340',
+    marginBottom: 20,
+  },
+  content: {
+    fontSize: 18,
+    color: '#0c2340',
+    marginBottom: 20,
+  },
+  rent: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#0c2340',
+  },
+});
+
 export default DetailsScreen;
+
 
--- frontend/screens/HomeScreen.js
+++ frontend/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity, StyleSheet, TextInput } from 'react-native';
 
 export default function HomeScreen({ navigation }) {
   const [posts, setPosts] = useState([
@@ -7,18 +7,26 @@ export default function HomeScreen({ navigation }) {
       id: 1,
       title: 'Post 1',
       content: 'This is the first post.',
+      author: 'User A',
+      rent: '$1000',
     },
     {
       id: 2,
       title: 'Post 2',
       content: 'This is the second post.',
+      author: 'User B',
+      rent: '$1000',
     },
     {
       id: 3,
       title: 'Post 3',
       content: 'This is the third post.',
+      author: 'User A',
+      rent: '$1000',
     },
   ]);
+  const [search, setSearch] = useState('');
+  const [currentUserPosts, setCurrentUserPosts] = useState(false);
 
   const handlePress = (post) => {
     navigation.navigate('Details', { post });
@@ -27,18 +35,42 @@ export default function HomeScreen({ navigation }) {
   const renderPost = ({ item }) => {
     return (
       <TouchableOpacity style={styles.post} onPress={() => handlePress(item)}>
-        <Text style={styles.postTitle}>{item.title}</Text>
-        <Text style={styles.postContent}>{item.content}</Text>
+        <View style={styles.rentContainer}>
+          <Text style={styles.rent}>{item.rent}</Text>
+        </View>
+        <View style={styles.postDetails}>
+          <Text style={styles.postTitle}>{item.title}</Text>
+          <Text style={styles.postContent}>{item.content}</Text>
+          <Text style={styles.postAuthor}>Posted by: {item.author}</Text>
+        </View>
       </TouchableOpacity>
     );
   };
+  
+
+  const filteredPosts = currentUserPosts
+    ? posts.filter((post) => post.author === 'User A') // replace with actual user id
+    : posts.filter((post) => post.title.toLowerCase().includes(search.toLowerCase()));
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Posts</Text>
-
+      <TextInput
+        placeholder="Search by title"
+        style={styles.searchInput}
+        onChangeText={(text) => setSearch(text)}
+        value={search}
+      />
+      <TouchableOpacity
+        style={styles.filterButton}
+        onPress={() => setCurrentUserPosts(!currentUserPosts)}
+      >
+        <Text style={styles.filterButtonText}>
+          {currentUserPosts ? 'Show all posts' : 'Show my posts'}
+        </Text>
+      </TouchableOpacity>
       <FlatList
-        data={posts}
+        data={filteredPosts}
         renderItem={renderPost}
         keyExtractor={(item) => item.id.toString()}
         style={styles.list}
@@ -61,6 +93,25 @@ const styles = StyleSheet.create({
     color: '#0F4D92',
     fontFamily: 'Avenir',
   },
+  searchInput: {
+    height: 40,
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: '#ccc',
+    paddingHorizontal: 10,
+    marginBottom: 10,
+  },
+  filterButton: {
+    backgroundColor: '#0F4D92',
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  filterButtonText: {
+    color: '#fff',
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
   list: {
     flex: 1,
   },
@@ -71,6 +122,19 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     borderWidth: 1,
     borderColor: '#0F4D92',
+    flexDirection: 'row',
+    justifyContent: 'space-between', // add this to align rent to right
+    alignItems: 'center', // add this to center vertically
+  },
+  postDetails: {
+    flex: 1,
+  },
+  postRent: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#0F4D92',
+    fontFamily: 'Avenir',
+    marginRight: 10,
   },
   postTitle: {
     fontSize: 18,
@@ -84,4 +148,18 @@ const styles = StyleSheet.create({
     color: '#555555',
     fontFamily: 'Avenir',
   },
+  rentContainer: {
+    backgroundColor: '#0F4D92',
+    borderRadius: 5,
+    padding: 5,
+    marginRight: 10,
+  },
+  rent: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    fontWeight: 'bold',
+    fontFamily: 'Avenir',
+  },
 });
+
+
