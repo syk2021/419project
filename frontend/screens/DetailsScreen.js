88df882251c9c9243f7d4b3d838772750d5b5acc
Lindsay <lindsay.chen@yale.edu>
2023-04-08 20:30:04 -0400

    updated details

--- frontend/screens/DetailsScreen.js
+++ frontend/screens/DetailsScreen.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { TextInput } from 'react-native';
+import { ScrollView } from 'react-native';
+
 
 const DetailsScreen = ({ route }) => {
   const { post } = route.params;
   const navigation = useNavigation();
+
+  // Define state to hold the user's comment and the list of existing comments
+  const [comment, setComment] = useState('');
+
+  const [comments, setComments] = useState([
+    {id: 1, author: 'John', text: 'This is a great post!'},
+    {id: 2, author: 'Jane', text: 'Thanks for sharing your thoughts.'},
+    {id: 3, author: 'Jane', text: 'Thanks for sharing your thoughts.'},
+
+  ]);
+
+  const handleAddComment = () => {
+  const newComment = { id: comments.length + 1, author: 'You', text: comment };
+  setComments([...comments, newComment]);
+  setComment('');
+  }
+
 
   return (
     <View style={styles.container}>
@@ -16,6 +36,31 @@ const DetailsScreen = ({ route }) => {
         <Text style={styles.author}>By {post.author}</Text>
         <Text style={styles.content}>{post.content}</Text>
         <Text style={styles.rent}>Rent: {post.rent}</Text>
+      </View>
+      <View style={styles.commentSection}>
+        <Text style={styles.commentTitle}>Comments:</Text>
+        <ScrollView>
+          {comments.map((comment) => (
+            <View key={comment.id} style={styles.commentContainer}>
+              <Text style={styles.commentAuthor}>{comment.author}: </Text>
+              <Text style={styles.commentText}>{comment.text}</Text>
+            </View>
+          ))}
+        </ScrollView>
+        <View style={styles.commentInputContainer}>
+        <TextInput 
+          style={styles.commentInput} 
+          placeholder="Write a comment..."
+          value={comment}
+          onChangeText={setComment}
+        />
+        <TouchableOpacity 
+          style={styles.commentButton}
+          onPress={handleAddComment}
+        >
+          <Text style={styles.commentButtonText}>Post</Text>
+        </TouchableOpacity>
+      </View>
       </View>
     </View>
   );
@@ -74,6 +119,53 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
     color: '#0c2340',
+  },
+  commentContainer: {
+    backgroundColor: '#ffffff',
+    borderRadius: 5,
+    padding: 20,
+    marginBottom: 20,
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: '#c5c5c5',
+  },
+  commentAuthor: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#0c2340',
+    marginBottom: 10,
+  },
+  commentText: {
+    fontSize: 18,
+    color: '#0c2340',
+    marginBottom: 20,
+  },
+  commentInputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  commentInput: {
+    flex: 1,
+    height: 40,
+    borderColor: '#c5c5c5',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginRight: 10,
+  },
+  commentButton: {
+    backgroundColor: '#0c2340',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#0c2340',
+  },
+  commentButtonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#ffffff',
   },
 });
 
